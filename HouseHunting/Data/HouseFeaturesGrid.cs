9aaba8ee2ec39836b3bd6841ef0375e9015f962a
Jennifer Kroll <jkroll@nailor.com>
2020-09-29 16:48:38 -0500

    new branch same error

---
+++ HouseHunting/Data/HouseFeaturesGrid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseHunting.Data
+{
+    public class HouseFeaturesGrid
+    {
+        public int HouseID { get; set; }
+        public int FeatureID { get; set; }
+       
+        public string Description { get; set; }
+        public int Weight { get; set; }
+        public bool IsEditable { get; set; }
+    }
+}
---
+++ SqlProject/StoredProcedures/spGetAllHouseFeatures.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spGetAllHouseFeatures]
+	
+AS
+Begin
+Set nocount on
+    select HouseFeatures.HouseID, HouseFeatures.FeatureID, Features.Description, Features.IsEditable,
+  Case when HouseFeatures.Weight is null then Features.Weight else HouseFeatures.Weight end As Weight
+  from HouseFeatures
+  Inner join Features on HouseFeatures.FeatureID = Features.FeatureID
+end
